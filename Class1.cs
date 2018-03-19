805f4f0e895673981c6d19e2232c2163310f817f
tjcold <tjcold@hotmail.com>
2018-03-19 20:59:53 +0100

    merged

--- Class1.cs
+++ Class1.cs
@@ -7,5 +7,10 @@ namespace tjukica_pis_1
 {
     class Class1
     {
+        public void meteoda()
+        {
+            Console.WriteLine("ovo je prazna metoda");
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace tjukica_pis_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            #KOMENTAR od Luke
-            #drugi kom
-            #treci kom
+            //KOMENTAR od Luke
+            //rugi kom
+            //treci kom
             Console.WriteLine("Maci");
             Console.WriteLine("Nemogu nikako sa git diff vidit promjenu koju ovdi napravim");
         }
